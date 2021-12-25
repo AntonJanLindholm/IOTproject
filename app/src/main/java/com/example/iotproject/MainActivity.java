b60ceac6f4d079623437c731d3106153840ff355
lindholmanton <lindholmjananton@gmail.com>
2021-12-25 11:28:28 +0100

    Re-done the gui

--- app/src/main/java/com/example/iotproject/MainActivity.java
+++ app/src/main/java/com/example/iotproject/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.iotproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView Trashcan_, Temp_, Lamp_;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lamp_ = (CardView) findViewById(R.id.Lamp);
+        Temp_ = (CardView) findViewById(R.id.Temp);
+        Trashcan_ = (CardView) findViewById(R.id.Trashcan);
+
+        Lamp_.setOnClickListener((View.OnClickListener) this);
+        Temp_.setOnClickListener((View.OnClickListener) this);
+        Trashcan_.setOnClickListener((View.OnClickListener) this);
+
+
+        }
+        @Override
+        public void onClick(View v) {
+        Intent i ;
+        switch ( v.getId() ){
+
+            case R.id.Lamp: i = new Intent(this,Lamp.class); startActivity(i); break;
+            case R.id.Temp: i = new Intent(this,Temp.class); startActivity(i); break;
+            case R.id.Trashcan: i = new Intent(this,Trashcan.class); startActivity(i); break;
+
+
+        }
+
     }
+
+
 }
